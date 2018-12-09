0b840bd8dfc1a80402405e7895175af413a0c826
Fabian Isai <fabian.isai@gmail.com>
2018-12-09 13:29:38 -0800

    Initial Commit

---
+++ Tab Bar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tab Bar
+//
+//  Created by FabianIsai on 12/9/18.
+//  Copyright © 2018 FabianIsai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
